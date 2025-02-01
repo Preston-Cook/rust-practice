9dd08d8b219f1076e99e7da2a39206929bd06b3f
Preston-Cook <preston.l.cook@gmail.com>
2025-02-01 15:08:23 -0600

    create number guessing game

--- chapter2/guessing_game/src/main.rs
+++ chapter2/guessing_game/src/main.rs
@@ -1,3 +1,38 @@
+use std::{
+    cmp::Ordering,
+    io::{stdin, stdout, Write},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let secret_num: i32 = rand::random_range(1..=100);
+
+    let mut guess: String;
+
+    loop {
+        guess = String::new();
+
+        print!("Enter guess: ");
+        stdout().flush().expect("Error: Failed to flush stdout");
+
+        stdin()
+            .read_line(&mut guess)
+            .expect("Error: Failed to read from stdin");
+
+        let guess_num: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a valid number!");
+                continue;
+            }
+        };
+
+        match guess_num.cmp(&secret_num) {
+            Ordering::Greater => println!("Too large!"),
+            Ordering::Less => println!("Too small!"),
+            Ordering::Equal => {
+                println!("Correct!");
+                break;
+            }
+        }
+    }
 }
